47878d9af428622ddade0e9bdcaf74974b56df28
yunxuan <yunxuan@didichuxing.com>
2019-06-22 21:24:15 +0800

    访问策略

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\User;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function update(User $currentUser, User $user)
+    {
+        return $currentUser->id === $user->id;
+    }
+}
